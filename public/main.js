8034c931c9202c1819642ae3841040d42a57e33f
cnfairall <101303134+cnfairall@users.noreply.github.com>
2023-09-05 21:49:09 -0500

    created DOM containers, fetch and render vocab

--- public/main.js
+++ public/main.js
@@ -2,9 +2,20 @@
 import ViewDirectorBasedOnUserAuthStatus from '../utils/viewDirector';
 import 'bootstrap'; // import bootstrap elements and js
 import '../styles/main.scss';
+import domBuilder from '../utils/domBuilder';
+import { getVocab } from '../api/vocabData';
+import { showVocab, emptyVocab } from '../pages/entries';
 
 const init = () => {
   ViewDirectorBasedOnUserAuthStatus();
+  domBuilder();
+  getVocab().then((array) => {
+    if (array.length) {
+      showVocab(array);
+    } else {
+      emptyVocab();
+    }
+  });
 };
 
 init();
